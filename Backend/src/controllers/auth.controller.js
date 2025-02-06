57715efef90d7edc110294635025ab9179df7a11
25shivansh <shivansh.singh4work@gmail.com>
2025-02-06 15:43:23 +0530

    final auth routes

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -109,5 +109,36 @@ export const logout=async (req,res)=>{
     }
 };
 export const updateProfile=async (req,res)=>{
+    try{
+        const {profilePic}=req.body;
+        const userId=req.user._id;
+        if(!profilePic){
+            return res.status(400).json({
+                message:"Profile Pic is required"
+            })
+        }
+        const uploadResponse=await cloudinary_uploader.upload(profilePic);
+        const updatedUser=await User.findByIdAndDelete(userId,
+            {profilePic:uploadResponse.secure_url},{new:true})
+        res.status(200).json({
+            updatedUser
+        })    
+    }catch(error){
+        console.log("error in update profile: ",error)
+        res.status(500).json({
+            message:"Internal server error"
+        })
 
+    }
+
+}
+export const checkAuth=(req,res)=>{
+    try{
+        res.status(200).json(req.user);
+    }catch(error){
+        console.log("Error in checkAuth controller ",error.message);
+        res.status(500).json({
+            message:"Internal Server Error"
+        })
+    }
 }
